af5a2747eff2dec163d5ba1650e767fece646fc3
Andrés Araya <araya.andres@gmail.com>
2025-03-09 18:04:04 -0600

    fix bug :)

--- mylib/fib.cc
+++ mylib/fib.cc
@@ -2,9 +2,10 @@
 
 unsigned fib(const unsigned n)
 {
+    if (n == 0) return 0;
     unsigned a = 0;
     unsigned b = 1;
-    for (unsigned i = 0; i < n; i++)
+    for (unsigned i = 1; i < n; i++)
     {
         unsigned c = a + b;
         a = b;
--- tests/tests.cc
+++ tests/tests.cc
@@ -5,3 +5,18 @@ TEST(mylib, f0_is_equal_to_0)
 {
     EXPECT_EQ(fib(0), 0);
 }
+
+TEST(mylib, f1_is_equal_to_1)
+{
+    EXPECT_EQ(fib(1), 1);
+}
+
+TEST(mylib, f2_is_equal_to_1)
+{
+    EXPECT_EQ(fib(2), 1);
+}
+
+TEST(mylib, f3_is_equal_to_2)
+{
+    EXPECT_EQ(fib(3), 2);
+}
